cd02ce260a1377604f5cf4060a9f2b2a67d6d1ee
Elerson Santos <elerson@gmail.com>
2021-09-05 13:39:46 -0300

    adding new robust

--- robust_loss_pytorch/distribution_skewed.py
+++ robust_loss_pytorch/distribution_skewed.py
@@ -236,7 +236,7 @@ class DistributionSkewed():
     x = torch.as_tensor(x)
     
     alpha_ = torch.as_tensor(alpha)
-    alpha  = torch.clamp(torch.as_tensor(alpha_[0][0])*gt*gt + torch.as_tensor(alpha_[0][1])*gt + torch.as_tensor(alpha_[0][2]), torch.as_tensor(-1.5), torch.as_tensor(1.5))
+    alpha  = torch.clamp(torch.as_tensor(alpha_[0][0])*gt*gt + torch.as_tensor(alpha_[0][1])*gt + torch.as_tensor(alpha_[0][2]), torch.as_tensor(-1.49), torch.as_tensor(1.49))
     
     #print('alpha', alpha.shape)
     
@@ -245,6 +245,9 @@ class DistributionSkewed():
     
     scale = torch.as_tensor(scale)
     assert (beta >= 0).all()
+    assert (beta <= 2.0).all()
+    assert (alpha >= -1.5).all()
+    assert (alpha <= 1.5).all()
     assert (scale >= 0).all()
     float_dtype = x.dtype
     assert beta.dtype == float_dtype
