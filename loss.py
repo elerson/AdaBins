715ae4bacb7129ce4fd583f4a13eda090a121502
Elerson Santos <elerson@gmail.com>
2021-09-06 12:18:01 -0300

    changing chanfer dist

--- loss.py
+++ loss.py
@@ -48,5 +48,6 @@ class BinsChamferLoss(nn.Module):  # Bin centers regularizer used in AdaBins pap
 
         dist1, dist2, idx1, idx2 = self.chamfer_dist(input_points, target_points)
         loss = (torch.mean(dist1)) + (torch.mean(dist2))
+        print('chang', loss)
         #loss, _ = chamfer_distance(x=input_points, y=target_points, y_lengths=target_lengths)
         return loss
