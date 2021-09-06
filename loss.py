f9ab3a3a37f1f360b026995708bf54872260198c
Elerson Santos <elerson@gmail.com>
2021-09-06 12:33:22 -0300

    changing chanfer dist

--- loss.py
+++ loss.py
@@ -1,8 +1,119 @@
 import torch
 import torch.nn as nn
-import ChamferDistancePytorch.chamfer2D.dist_chamfer_2D as chamfer2D
+#import ChamferDistancePytorch.chamfer2D.dist_chamfer_2D as chamfer2D
 from torch.nn.utils.rnn import pad_sequence
 
+
+def chamfer_distance_without_batch(p1, p2, debug=False):
+
+    '''
+    Calculate Chamfer Distance between two point sets
+    :param p1: size[1, N, D]
+    :param p2: size[1, M, D]
+    :param debug: whether need to output debug info
+    :return: sum of Chamfer Distance of two point sets
+    '''
+
+    assert p1.size(0) == 1 and p2.size(0) == 1
+    assert p1.size(2) == p2.size(2)
+
+    if debug:
+        print(p1[0][0])
+
+    p1 = p1.repeat(p2.size(1), 1, 1)
+    if debug:
+        print('p1 size is {}'.format(p1.size()))
+
+    p1 = p1.transpose(0, 1)
+    if debug:
+        print('p1 size is {}'.format(p1.size()))
+        print(p1[0])
+
+    p2 = p2.repeat(p1.size(0), 1, 1)
+    if debug:
+        print('p2 size is {}'.format(p2.size()))
+        print(p2[0])
+
+    dist = torch.add(p1, torch.neg(p2))
+    if debug:
+        print('dist size is {}'.format(dist.size()))
+        print(dist[0])
+
+    dist = torch.norm(dist, 2, dim=2)
+    if debug:
+        print('dist size is {}'.format(dist.size()))
+        print(dist)
+
+    dist = torch.min(dist, dim=1)[0]
+    if debug:
+        print('dist size is {}'.format(dist.size()))
+        print(dist)
+
+    dist = torch.sum(dist)
+    if debug:
+        print('-------')
+        print(dist)
+
+    return dist
+
+
+def chamfer_distance_with_batch(p1, p2, debug):
+
+    '''
+    Calculate Chamfer Distance between two point sets
+    :param p1: size[B, N, D]
+    :param p2: size[B, M, D]
+    :param debug: whether need to output debug info
+    :return: sum of all batches of Chamfer Distance of two point sets
+    '''
+
+    assert p1.size(0) == p2.size(0) and p1.size(2) == p2.size(2)
+
+    if debug:
+        print(p1[0])
+
+    p1 = p1.unsqueeze(1)
+    p2 = p2.unsqueeze(1)
+    if debug:
+        print('p1 size is {}'.format(p1.size()))
+        print('p2 size is {}'.format(p2.size()))
+        print(p1[0][0])
+
+    p1 = p1.repeat(1, p2.size(2), 1, 1)
+    if debug:
+        print('p1 size is {}'.format(p1.size()))
+
+    p1 = p1.transpose(1, 2)
+    if debug:
+        print('p1 size is {}'.format(p1.size()))
+        print(p1[0][0])
+
+    p2 = p2.repeat(1, p1.size(1), 1, 1)
+    if debug:
+        print('p2 size is {}'.format(p2.size()))
+        print(p2[0][0])
+
+    dist = torch.add(p1, torch.neg(p2))
+    if debug:
+        print('dist size is {}'.format(dist.size()))
+        print(dist[0])
+
+    dist = torch.norm(dist, 2, dim=3)
+    if debug:
+        print('dist size is {}'.format(dist.size()))
+        print(dist)
+
+    dist = torch.min(dist, dim=2)[0]
+    if debug:
+        print('dist size is {}'.format(dist.size()))
+        print(dist)
+
+    dist = torch.sum(dist)
+    if debug:
+        print('-------')
+        print(dist)
+
+    return dist
 
 class SILogLoss(nn.Module):  # Main loss function used in AdaBins paper
     def __init__(self):
@@ -45,9 +156,9 @@ class BinsChamferLoss(nn.Module):  # Bin centers regularizer used in AdaBins pap
 
 
         
-
-        dist1, dist2, idx1, idx2 = self.chamfer_dist(input_points, target_points)
-        loss = (torch.mean(dist1)) + (torch.mean(dist2))
-        print('chang', loss)
+        
+        #dist1, dist2, idx1, idx2 = self.chamfer_dist(input_points, target_points)
+        loss = chamfer_distance_with_batch(input_points, target_points)
+        #print('chang', loss)
         #loss, _ = chamfer_distance(x=input_points, y=target_points, y_lengths=target_lengths)
         return loss
