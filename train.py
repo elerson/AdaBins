e13a805c9e93344fccd8fb1610c66514a28a0935
Elerson R. S. Santos <elerss@gmail.com>
2021-09-11 01:03:10 -0300

    small error

--- train.py
+++ train.py
@@ -200,7 +200,7 @@ def train(model, args, epochs=10, experiment_name="DeepLab", lr=0.0001, root="."
 
             loss = l_dense + args.w_chamfer * l_chamfer
             loss.backward()
-            nn.utils.clip_grad_norm_([model.parameters(), adaptive_image_loss_func.parameters()], 0.1)  # optional
+            #nn.utils.clip_grad_norm_([model.parameters(), adaptive_image_loss_func.parameters()], 0.1)  # optional
             optimizer.step()
             if should_log and step % 5 == 0:
                 wandb.log({f"Train/{criterion_ueff.name}": l_dense.item()}, step=step)
