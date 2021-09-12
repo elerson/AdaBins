944e69bf62986c3a54a7fd2bf1233db606bf6ad1
Elerson R. S. Santos <elerss@gmail.com>
2021-09-12 14:17:06 -0300

    adding new loss

--- train.py
+++ train.py
@@ -76,9 +76,7 @@ def main_worker(gpu, ngpus_per_node, args):
     model = models.UnetAdaptiveBins.build(n_bins=args.n_bins, min_val=args.min_depth, max_val=args.max_depth,
                                           norm=args.norm)
 
-    print(args.resume)
-    model, _, _ = model_io.load_checkpoint(args.resume, model)
-    print('loaded')
+
 
     ################################################################################################
 
@@ -176,7 +174,9 @@ def train(model, args, epochs=10, experiment_name="DeepLab", lr=0.0001, root="."
                                               div_factor=args.div_factor,
                                               final_div_factor=args.final_div_factor)
     if args.resume != '' and scheduler is not None:
-
+        print(args.resume)
+        model, _, _ = model_io.load_checkpoint(args.resume, model)
+        print('loaded')
         scheduler.step(args.epoch + 1)
         
     ################################################################################################
