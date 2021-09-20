e292148f7f8a1da7c9c6ade15ac7275d63e8a808
Elerson R. S. Santos <elerss@gmail.com>
2021-09-20 11:50:52 -0300

    adding noise to data

--- dataloader.py
+++ dataloader.py
@@ -17,7 +17,7 @@ class AddGaussianNoise(object):
         self.mean = mean
         
     def __call__(self, tensor):
-        return tensor + torch.randn(tensor.shape) * self.std + self.mean
+        return tensor['depth'] + torch.randn(tensor['depth'].size()) * self.std + self.mean
     
     def __repr__(self):
         return self.__class__.__name__ + '(mean={0}, std={1})'.format(self.mean, self.std)
