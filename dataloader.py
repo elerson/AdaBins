8b6a8c607f7defc53bedefcad8150b3fe4bc29e9
Elerson R. S. Santos <elerss@gmail.com>
2021-09-20 11:55:23 -0300

    adding noise to data

--- dataloader.py
+++ dataloader.py
@@ -48,7 +48,7 @@ def preprocessing_transforms_train(mode):
 
 class DepthDataLoader(object):
     def __init__(self, args, mode):
-        if mode == 'train' or mode == 'train_validation':
+        if mode == 'train':
             self.training_samples = DataLoadPreprocess(args, mode, transform=preprocessing_transforms_train(mode))
             if args.distributed:
                 self.train_sampler = torch.utils.data.distributed.DistributedSampler(self.training_samples)
@@ -60,6 +60,18 @@ class DepthDataLoader(object):
                                    num_workers=args.num_threads,
                                    pin_memory=True,
                                    sampler=self.train_sampler)
+        elif mode == 'train_validation':
+            self.training_samples = DataLoadPreprocess(args, mode, transform=preprocessing_transforms(mode))
+            if args.distributed:
+                self.train_sampler = torch.utils.data.distributed.DistributedSampler(self.training_samples)
+            else:
+                self.train_sampler = None
+
+            self.data = DataLoader(self.training_samples, args.batch_size,
+                                   shuffle=(self.train_sampler is None),
+                                   num_workers=args.num_threads,
+                                   pin_memory=True,
+                                   sampler=self.train_sampler)        
 
         elif mode == 'online_eval':
             self.testing_samples = DataLoadPreprocess(args, mode, transform=preprocessing_transforms(mode))
